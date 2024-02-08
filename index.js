005e41b6ea7c4017382457711a11b852f535ea9d
Kia Neitzell <kia.neitzell@gmail.com>
2024-02-07 20:30:10 -0800

    compleated

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+function titleCased() {
+  const tutorialNames = [
+    "What Does The This Keyword Mean?",
+    "What Is The Constructor OO Pattern?",
+    "Implementing Blockchain Web API",
+    "The test driven development workflow",
+    "What is NaN and how can we check for it",
+    "What is the difference between stopPropagation and preventDefault?",
+    "Immutable state and pure functions",
+    "What is the difference between == and ===?",
+    "What is the difference between event capturing and bubbling?",
+    "What is JSONP?"
+  ];
 
-const titleCased = () => {
-  return tutorials
+  function titleCase(str) {
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  }
+
+  const titleCasedTutorials = tutorialNames.map(tutorial => titleCase(tutorial));
+
+
+  return titleCasedTutorials;
 }
+
+console.log(titleCased());
